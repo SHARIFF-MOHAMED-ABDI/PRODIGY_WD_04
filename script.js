c1947fbd3f1468d2a5c6295226566a7715d8c655
SHARIFF MOHAMED ABDI <shariffmohamedabdi@gmail.com>
2024-12-03 21:11:49 +0300

    add new comment

---
+++ script.js
@@ -0,0 +1,8 @@
+// Example JavaScript for simple interactivity
+
+document.addEventListener('DOMContentLoaded', () => {
+  const ctaButton = document.querySelector('.cta-btn');
+  ctaButton.addEventListener('click', () => {
+      alert("Thanks for exploring my work!");
+  });
+});
